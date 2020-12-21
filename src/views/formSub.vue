eb511d9fb9ac477236bd877a02a16afb8bf66339
xinshaoguang <997888255@qq.com>
2020-12-21 09:06:28 +0800

    添加table,form

---
+++ src/views/formSub.vue
@@ -0,0 +1,44 @@
+<template>
+  <a-form-item label="Gender">
+    <a-input
+    @blur="abc"
+      v-decorator="[
+        'gender',
+        { rules: [{ required: true, message: 'Please input your gender!' }] },
+      ]"
+    >
+ 
+    </a-input>
+  </a-form-item>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+    //   formLayout: 'horizontal',
+    //   form: this.$form.createForm(this, { name: 'coordinated' }),
+    };
+  },
+  methods: {
+      abc(){
+        console.log(1)
+        this.$emit('handle','gender','xin')
+      }
+    // handleSubmit(e) {
+    //   e.preventDefault();
+    //   this.form.validateFields((err, values) => {
+    //     if (!err) {
+    //       console.log('Received values of form: ', values);
+    //     }
+    //   });
+    // },
+    // handleSelectChange(value) {
+    //   console.log(value);
+    //   this.form.setFieldsValue({
+    //     note: `Hi, ${value === 'male' ? 'man' : 'lady'}!`,
+    //   });
+    // },
+  },
+};
+</script>
